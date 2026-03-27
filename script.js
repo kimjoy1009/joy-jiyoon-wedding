3f49103fc0cbe83523665d0b3c345bbc891efe88
kimjoy1009 <kimjoy1009@gmail.com>
2026-03-26 23:34:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -216,9 +216,17 @@
     }
 
     const parentsHTML = `
-      <p class="parent-line">${parentSpan(g.father, g.fatherDeceased)} · ${parentSpan(g.mother, g.motherDeceased)}의 아들 <span class="child-name">${g.name}</span></p>
-      <p class="parent-line">${parentSpan(b.father, b.fatherDeceased)} · ${parentSpan(b.mother, b.motherDeceased)}의 딸 <span class="child-name">${b.name}</span></p>
-    `;
+  <p class="parent-line">
+    <span class="parent-left">${parentSpan(g.father, g.fatherDeceased)} · ${parentSpan(g.mother, g.motherDeceased)}</span>
+    <span class="parent-mid">의 아들</span>
+    <span class="parent-right child-name">${g.name}</span>
+  </p>
+  <p class="parent-line">
+    <span class="parent-left">${parentSpan(b.father, b.fatherDeceased)} · ${parentSpan(b.mother, b.motherDeceased)}</span>
+    <span class="parent-mid">의 딸</span>
+    <span class="parent-right child-name">${b.name}</span>
+  </p>
+`;
     $('#heroParents').innerHTML = parentsHTML;
 
     // Fix mobile viewport height
